90c86036b604d22038c2098667e6cfe3f240dce6
breezedemar <brissa.hernandez2021@gmail.com>
2022-07-08 21:10:36 -0500

    first commit rama 07

---
+++ js/arrowFunctions.js
@@ -0,0 +1,13 @@
+//! Arrow functions 
+
+/*Funciones flecha o arrow functions 
+    *No necesitamos de la palabra reservada function
+    *No en todos los casos necesitamos la palabra reservada return para regresar
+ */
+
+const arrowFunction = (p1) => {
+    console.log(p1);
+}
+
+console.log("✈ 💙 ")
+
--- js/practicas.js
+++ js/practicas.js
@@ -188,7 +188,7 @@ Condiciones, tiene argumentos y tiene que hacer un return*/
 */
 
 
-function multiplo(){
+/*function multiplo(){
 let suma = 0
 for(let i=1; i<=100; i++){
     
@@ -201,6 +201,8 @@ return suma
 }
 
 console.log(multiplo()); 
+
+*/
 
 
 //? Ejercicio 2
@@ -226,3 +228,41 @@ console.log(multiplo());
 * ->  E -> 1
 * ->  Espacios -> 1
 */
+
+
+//! Sesión 08 JavaScript 
+/** Pratica grupal
+Tomar 2 ejercicios del dia de ayer dy refactorizar a arrow functions*/
+
+/* Practica Grupal 2
+Arrow function
+Pedir al usuario una cadena de texto
+Declarar una función flecha para
+Pasarle la oración del usuario como parámetro de la función declarada
+verificar si esa oración es un palíndromo
+Retornar el resultado y mostrarlo al usuario (usando log o aler)
+Por ejemplo:
+// isPalindrome('hola') -> No es un palíndromo
+// isPalindrome('oso') -> SI es un palindromo
+ 
+   Podemos probar con las siguientes ->
+   anita lava la tina -> Palindromo
+   oso -> Palindromo
+   seres -> Palindromo
+ 
+   Hint / Pista
+    Quitamos los espacios
+
+Enviar un mensaje a 20a-generación
+
+*/ 
+
+let text = prompt("Ingresa un texto")
+
+const palindrom = (cadena) => {
+    const cadenaReversed = cadena.split("").reverse().join("");
+  
+    return cadenaReversed === cadena ? "es palindromo" : "no es palindromo";
+  }
+
+console.log(palindrom (text))
